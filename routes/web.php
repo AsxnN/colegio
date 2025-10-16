203af3ae87413609d8d1a81e4bdc1753946a0793
karoandrea <karolandrearamirez14@gmail.com>
2025-10-16 12:20:11 -0500

    vista estudiantes, seede, web

--- routes/web.php
+++ routes/web.php
@@ -20,23 +20,36 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 
+    Route::middleware(['role:administrador'])->group(function () {
+
     // Rutas para el módulo de usuarios
     Route::resource('usuarios', UsersController::class);
-    
+
     // Rutas para el módulo de administradores
     Route::resource('administradores', AdministradoresController::class);
-    
+
     // Rutas para el módulo de docentes
     Route::resource('docentes', DocentesController::class);
-    
+
     // Rutas para el módulo de estudiantes
     Route::resource('estudiantes', EstudiantesController::class);
     Route::get('/estudiantes-estadisticas', [EstudiantesController::class, 'estadisticas'])->name('estudiantes.estadisticas');
-    
+
     // Rutas para el módulo de secciones
     Route::resource('secciones', SeccionesController::class);
     Route::get('/secciones/{seccione}/estudiantes', [SeccionesController::class, 'estudiantes'])->name('secciones.estudiantes');
     Route::post('/secciones/{seccione}/asignar-estudiante', [SeccionesController::class, 'asignarEstudiante'])->name('secciones.asignar-estudiante');
     Route::delete('/secciones/{seccione}/remover-estudiante', [SeccionesController::class, 'removerEstudiante'])->name('secciones.remover-estudiante');
     Route::put('/secciones/{seccione}/transferir-estudiante', [SeccionesController::class, 'transferirEstudiante'])->name('secciones.transferir-estudiante');
+    });
+
+    // Rutas para el estudiante
+    Route::middleware(['role:estudiante'])->group(function () {
+        Route::get('/estudiantes/{id}/perfil', [EstudiantesController::class, 'perfil'])->name('estudiante.perfil');
+        Route::get('/estudiantes/{id}/cursos', [EstudiantesController::class, 'cursos'])->name('estudiante.cursos');
+        Route::get('/estudiantes/{id}/notas', [EstudiantesController::class, 'notas'])->name('estudiante.notas');
+        Route::get('/estudiantes/{id}/predicciones', [EstudiantesController::class, 'predicciones'])->name('estudiante.predicciones');
+        Route::get('/estudiantes/{id}/recomendaciones', [EstudiantesController::class, 'recomendaciones'])->name('estudiante.recomendaciones');
+        Route::get('/estudiantes/{id}/recursos', [EstudiantesController::class, 'recursos'])->name('estudiante.recursos');
+    });
 });
